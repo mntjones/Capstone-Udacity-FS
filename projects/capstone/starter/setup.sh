9abed5553f99879302fa1b935b13bb0a035f83a7
Monica Tamaru Jones <mntamaru@gmail.com>
2023-11-03 15:38:34 -0700

    updated DB url

--- projects/capstone/starter/setup.sh
+++ projects/capstone/starter/setup.sh
@@ -1,1 +1,4 @@
-
+#!/bin/bash
+export DATABASE_URL="postgres://psql_capstone_deploy_user:eFLWlQRVk99gKLmxaOpgMAG6lkt0OVok@dpg-cl2ms9auuipc73805dug-a/psql_capstone_deploy"
+export EXCITED="true"
+echo "setup.sh script executed successfully!"
